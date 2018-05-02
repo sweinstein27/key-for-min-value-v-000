dbd84f84a4858b81959ee1bd915b99325c57ac91
Scott Weinstein <sweinstein27@gmail.com>
2018-05-02 15:29:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,8 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  name_hash.each{ |key, value| 
-    binding.pry 
-    value}
+  name_hash.each do |key, value|
+    binding.pry
+    value
+  end
 end
